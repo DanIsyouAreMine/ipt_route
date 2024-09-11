bde37c9435afa90936a6e8f490074079ae2ba041
DanIsyouAreMine <danchristianaguillon9@gmail.com>
2024-09-11 17:50:38 +0800

    Prelim

---
+++ resources/views/picture.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Picture Page')
+
+@section('content')
+    <h1>Home Page</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('home') }}">Home</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">Home Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('item') }}">Item Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('picture') }}">Picture Page</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/profile', function(){
+return view('pages.home');
+});
+
+Route::get('/profile', function(){
+    return view('pages.item');
+    });
+Route::get('/profile', function(){
+    return view('pages.picture');
+    });
+    
+
+?>
+
